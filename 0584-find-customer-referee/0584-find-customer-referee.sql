0a345f681e1b71fe0a78c09b074f8b7e75ea1433
Pushkar Bharuka <pushkargbharuka@gmail.com>
2026-07-27 18:07:01 +0530

    Time: 1293 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,7 +1,2 @@
 # Write your MySQL query statement below
-SELECT 
-    name
-FROM
-    Customer
-WHERE
-    referee_id != 2 IS NOT FALSE /*becz comparing anything will NULL returns UNKNOWN , not true or false*/
+SELECT name FROM Customer WHERE referee_id != 2 IS NOT FALSE /*becz comparing anything will NULL returns UNKNOWN , not true or false*/
