23e530459978527d002ef20ae4fad71cd229fa0d
Pushkar Bharuka <pushkargbharuka@gmail.com>
2026-07-28 07:29:28 +0530

    Time: 0 ms (100%), Space: 42.9 MB (82.17%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -7,11 +7,10 @@ class Solution {
                 cnt++;
         }
 
-        if(cnt>0){ // if there are breaks which means possibility of rotation
-             if(arr[n-1] > arr[0]){ //breaks the sorting order , even if it is rotated
+        if(arr[n-1] > arr[0]){ //breaks the sorting order , even if it is rotated
                  cnt++;
-             }
         }
+       
        
         
         /* There can only be one break , which will between last and 1st even in rotated array , but if more than one is found its not sorted  */
