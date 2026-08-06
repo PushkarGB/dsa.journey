8c710d5e2917916e8fc6c90b8cca0fb2f22a9bba
Pushkar Bharuka <pushkargbharuka@gmail.com>
2026-08-06 18:34:49 +0530

    Time: 64 ms (13.18%), Space: 47.8 MB (7.59%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -6,16 +6,14 @@ class Solution {
         int maxLen = 0;
         int left = 0;
         for (int i = 0; i < s.length(); i++) {
-             if (window.contains(s.charAt(i))) {
-                maxLen = Math.max(maxLen, window.size());
-               while(window.contains(s.charAt(i))){
+            char ch = s.charAt(i);
+            while(window.contains(ch)){
                     window.remove(s.charAt(left));
                     left++;
-               } 
             }
-            window.add(s.charAt(i));   
+            window.add(ch);
+            maxLen = Math.max(maxLen, i - left + 1);
         }
-        maxLen = Math.max(maxLen, window.size());
         return maxLen;
     }
 }
