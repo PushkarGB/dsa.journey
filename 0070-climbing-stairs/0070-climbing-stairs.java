a29b10a90da7b917da597a8ce332ab66cba69eab
Pushkar Bharuka <pushkargbharuka@gmail.com>
2026-08-06 10:35:35 +0530

    Time: 0 ms (100%), Space: 42.2 MB (36.65%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,4 +1,6 @@
 class Solution {
+    /*
+    //Memorization
     public int climbStairs(int n) {
         int[] dp = new int[n+1];
         for(int i = 0; i<n+1; i++){
@@ -20,6 +22,17 @@ class Solution {
         
         dp[n] = ways(n-1,dp) + ways(n-2,dp);
 
+        return dp[n];
+    }
+    */
+    //Tabulation
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
         return dp[n];
     }
 }
