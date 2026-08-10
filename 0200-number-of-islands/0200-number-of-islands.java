220b7414916844bb79d98e4f19fec6d368e8d5a1
Pushkar Bharuka <pushkargbharuka@gmail.com>
2026-08-10 23:03:53 +0530

    Time: 3 ms (87.48%), Space: 52.4 MB (46.45%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,11 +2,7 @@ class Solution {
     public int numIslands(char[][] grid) {
 
         int m = grid.length;
-        boolean[][] visited = new boolean[m][];
         //Directions - left,up,right,down
-        for (int i = 0; i < m; i++) {
-            visited[i] = new boolean[grid[i].length];
-        }
         int[] dx = { 0, -1, 0, 1 };
         int[] dy = { -1, 0, 1, 0 };
         int islands = 0;
@@ -14,9 +10,9 @@ class Solution {
         for (int i = 0; i < m; i++) {
             int n = grid[i].length;
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1' && !visited[i][j]) {
+                if (grid[i][j] == '1') {
                     islands++;
-                    completeIsland(grid, i, j, m, dx, dy, visited);
+                    completeIsland(grid, i, j, m, dx, dy);
                 }
             }
         }
@@ -24,18 +20,12 @@ class Solution {
         return islands;
     }
 
-    public void completeIsland(char[][] grid, int i, int j, int m, int[] dx, int[] dy, boolean[][] visited) {
+    public void completeIsland(char[][] grid, int i, int j, int m, int[] dx, int[] dy) {
         if (i < 0 || i >= m || j < 0 || j >= grid[i].length || grid[i][j] == '0')
             return;
-
-        if (visited[i][j])
-            return;
-
-        visited[i][j] = true;
-
+        grid[i][j] = '0';
         for (int k = 0; k < 4; k++) {
-            completeIsland(grid, i + dx[k], j + dy[k],m, dx, dy, visited);
+            completeIsland(grid, i + dx[k], j + dy[k],m, dx,dy);
         }
     }
-
 }
