1e5fc1edeb17621a74b79892df0128da00fa75da
Pushkar Bharuka <pushkargbharuka@gmail.com>
2026-08-30 00:54:03 +0530

    Time: 253 ms (77.64%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,10 +1,23 @@
+-- SELECT
+--     s.machine_id,
+--     ROUND(AVG(e.timestamp - s.timestamp),3) as processing_time
+-- FROM Activity s
+-- JOIN Activity e
+--     ON s.machine_id = e.machine_id
+--    AND s.process_id = e.process_id
+--    AND s.activity_type = 'start'
+--    AND e.activity_type = 'end'
+-- Group by machine_id;
+
 SELECT
-    s.machine_id,
-    ROUND(AVG(e.timestamp - s.timestamp),3) as processing_time
-FROM Activity s
-JOIN Activity e
-    ON s.machine_id = e.machine_id
-   AND s.process_id = e.process_id
-   AND s.activity_type = 'start'
-   AND e.activity_type = 'end'
-Group by machine_id;
+    machine_id,
+    ROUND(AVG(processing_time), 3) AS processing_time
+FROM (
+    SELECT
+        machine_id,
+        process_id,
+        MAX(timestamp) - MIN(timestamp) AS processing_time
+    FROM Activity
+    GROUP BY machine_id, process_id
+) AS t
+GROUP BY machine_id;
