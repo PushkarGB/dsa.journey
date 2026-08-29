94f837816e8ec75944ad4da3d56b857abe6f95c0
Pushkar Bharuka <pushkargbharuka@gmail.com>
2026-08-29 22:37:09 +0530

    Time: 536 ms (45.29%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,9 +1,15 @@
 # Write your MySQL query statement below
-select w.id 
-from Weather w 
-where w.temperature > 
-    ( 
-    select w1.temperature
-    from Weather w1
-    where DateDiff(w.recordDate,w1.recordDate) = 1
-    ) 
+-- select w.id 
+-- from Weather w 
+-- where w.temperature > 
+--     ( 
+--     select w1.temperature
+--     from Weather w1
+--     where DateDiff(w.recordDate,w1.recordDate) = 1
+--     ) 
+
+select today.id
+from weather today
+join weather yesterday
+    on DateDiff(today.recordDate,yesterday.recordDate) = 1
+where today.temperature > yesterday.temperature;
