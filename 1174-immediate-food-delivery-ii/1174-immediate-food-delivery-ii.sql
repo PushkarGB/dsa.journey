21e0d99ee39ee1229cab5ff7aa8e3802d2d6c61b
Pushkar Bharuka <pushkargbharuka@gmail.com>
2026-08-30 05:08:24 +0530

    Time: 682 ms (60.48%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,27 +1,36 @@
 # Write your MySQL query statement below
+-- select ROUND(
+--     AVG(d.is_Immediate)*100,
+--     2
+-- ) as immediate_percentage
+-- from
+-- (
 -- select 
---     delivery_id,
---     customer_id,
---     MIN(order_date) as first_order_date,
---     MIN(customer_pref_delivery_date) as first_pref_d_date
+--     (
+--         case 
+--             when MIN(order_date) =  MIN(customer_pref_delivery_date)
+--                 then 1
+--             else 0
+--         end
+--     ) is_immediate 
 -- from 
 --     Delivery
--- Group by customer_id
-select ROUND(
-    AVG(d.is_Immediate)*100,
-    2
-) as immediate_percentage
-from
-(
-select 
-    (
-        case 
-            when MIN(order_date) =  MIN(customer_pref_delivery_date)
-                then 1
-            else 0
-        end
-    ) is_immediate 
-from 
-    Delivery
-Group by customer_id 
+-- Group by customer_id 
+-- ) d
+
+SELECT
+    ROUND(
+        AVG(order_date = customer_pref_delivery_date) * 100,
+        2
+    ) AS immediate_percentage
+FROM (
+    SELECT
+        order_date,
+        customer_pref_delivery_date,
+        ROW_NUMBER() OVER (
+            PARTITION BY customer_id
+            ORDER BY order_date
+        ) AS rn
+    FROM Delivery
 ) d
+WHERE rn = 1;
