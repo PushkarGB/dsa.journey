74011fac2c688b80f894c44a608aa0f89ee6e225
Pushkar Bharuka <pushkargbharuka@gmail.com>
2026-08-29 20:15:29 +0530

    Time: 832 ms (48.61%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -8,15 +8,29 @@
 --     where e1.departmentId = emp.departmentId
 -- );
 
-select dept.name as Department, emp.name as Employee , emp.salary as Salary
-from Employee emp
-Join (
-    Select departmentId , Max(salary) as max_salary
-    from Employee
-    Group by departmentId
-) m 
-    on emp.departmentid = m.departmentid
-    And emp.salary = m.max_salary
-JOIN Department dept
-    ON dept.id = emp.departmentid;
+-- select dept.name as Department, emp.name as Employee , emp.salary as Salary
+-- from Employee emp
+-- Join (
+--     Select departmentId , Max(salary) as max_salary
+--     from Employee
+--     Group by departmentId
+-- ) m 
+--     on emp.departmentid = m.departmentid
+--     And emp.salary = m.max_salary
+-- JOIN Department dept
+--     ON dept.id = emp.departmentid;
+
+select dept.name as Department, e.name as Employee , e.salary as Salary
+from 
+(
+    select 
+        e.*,
+        MAX(salary) over(
+            PARTITION BY departmentid 
+        ) as max_salary
+    From Employee e
+) e
+Join Department dept 
+    on dept.id = e.departmentid
+    Where e.salary = e.max_salary;
 
