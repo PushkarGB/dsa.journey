39ab6aafb0e945ab9aafc140dcd35e6775ab8bdb
Pushkar Bharuka <pushkargbharuka@gmail.com>
2026-08-30 05:23:01 +0530

    Time: 534 ms (92.49%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,19 +1,35 @@
 # Write your MySQL query statement below
-Select 
-Round(
-    Count(player_id) / ( Select Count( Distinct player_id) from Activity ),
-    2
-) as fraction
-from (
-    SELECT Distinct f.player_id 
-    FROM (
-        SELECT
-            player_id,
-            MIN(event_date) AS first_date
-        FROM Activity
-        GROUP BY player_id
-    ) f
-    JOIN Activity a
-        ON a.player_id = f.player_id
-    AND a.event_date = DATE_ADD(f.first_date, INTERVAL 1 DAY)
+-- Select 
+-- Round(
+--     Count(player_id) / ( Select Count( Distinct player_id) from Activity ),
+--     2
+-- ) as fraction
+-- from (
+--     SELECT Distinct f.player_id 
+--     FROM (
+--         SELECT
+--             player_id,
+--             MIN(event_date) AS first_date
+--         FROM Activity
+--         GROUP BY player_id
+--     ) f
+--     JOIN Activity a
+--         ON a.player_id = f.player_id
+--     AND a.event_date = DATE_ADD(f.first_date, INTERVAL 1 DAY)
+-- ) f
+
+SELECT
+    ROUND(
+        SUM(a.event_date IS NOT NULL) / COUNT(*),
+        2
+    ) AS fraction
+FROM (
+    SELECT
+        player_id,
+        MIN(event_date) AS first_date
+    FROM Activity
+    GROUP BY player_id
 ) f
+LEFT JOIN Activity a
+    ON a.player_id = f.player_id
+   AND a.event_date = DATE_ADD(f.first_date, INTERVAL 1 DAY);
