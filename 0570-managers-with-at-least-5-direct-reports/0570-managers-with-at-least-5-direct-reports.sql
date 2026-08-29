94a1c07b21b9371a9f8462c06a95b8797de91e9e
Pushkar Bharuka <pushkargbharuka@gmail.com>
2026-08-30 01:52:34 +0530

    Time: 359 ms (72.41%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -6,10 +6,21 @@
 -- group by m.id
 -- Having count(*) >= 5
 
+-- SELECT m.name
+-- FROM Employee m
+-- WHERE (
+--     SELECT COUNT(*)
+--     FROM Employee e
+--     WHERE e.managerId = m.id
+-- ) >= 5;
+
 SELECT m.name
 FROM Employee m
-WHERE (
-    SELECT COUNT(*)
-    FROM Employee e
-    WHERE e.managerId = m.id
-) >= 5;
+JOIN (
+    SELECT managerId
+    FROM Employee
+    WHERE managerId IS NOT NULL
+    GROUP BY managerId
+    HAVING COUNT(*) >= 5
+) x
+ON m.id = x.managerId;
