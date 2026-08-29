221c6e6b7f6569684930f13269376ca7e74d9520
Pushkar Bharuka <pushkargbharuka@gmail.com>
2026-08-30 00:49:30 +0530

    Time: 264 ms (63.06%), Space: 0B (100%) - LeetHub

---
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -0,0 +1,10 @@
+SELECT
+    s.machine_id,
+    ROUND(AVG(e.timestamp - s.timestamp),3) as processing_time
+FROM Activity s
+JOIN Activity e
+    ON s.machine_id = e.machine_id
+   AND s.process_id = e.process_id
+   AND s.activity_type = 'start'
+   AND e.activity_type = 'end'
+Group by machine_id;
