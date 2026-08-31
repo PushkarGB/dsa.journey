0e141c44f4e09b8b9c098624fc40b543a7b73cb8
Pushkar Bharuka <pushkargbharuka@gmail.com>
2026-08-31 07:48:18 +0530

    Time: 588 ms (62.78%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,17 +1,15 @@
 # Write your MySQL query statement below
--- select employee_id , department_id
--- from Employee e
--- where primary_flag = 'Y' 
--- or (
---     Select Count(*) from Employee e2
---     where e2.employee_id = e.employee_id
--- ) = 1;
-
-SELECT
+select 
     employee_id,
     COALESCE(
-        MAX(CASE WHEN primary_flag = 'Y' THEN department_id END),
+        MAX(
+            CASE 
+                WHEN primary_flag = 'Y'
+                    THEN department_id
+                ElSE null
+            END 
+        ),
         MIN(department_id)
-    ) AS department_id
-FROM Employee
-GROUP BY employee_id;
+    ) as department_id
+from Employee
+group by employee_id
